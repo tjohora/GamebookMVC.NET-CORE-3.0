f372ae9de7348b9fa553154f66db6f09b96deefa
D00078580 <60467492+D00078580@users.noreply.github.com>
2020-05-06 11:57:00 +0100

    Comments Commit

---
+++ CA3_TATJ_V2/Models/CommentRepository.cs
@@ -0,0 +1,46 @@
+﻿using CA3_TATJ_V2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA3_TATJ_V2.Models
+{
+   public class CommentRepository : ICommentRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public IEnumerable<Comment> allComments
+        {
+            get
+            {
+                return _context.Comments;
+            }
+        }
+
+        //public IEnumerable<Comment> getCommentsByPostID(int postID)
+        //{
+        //    {
+        //        return _context.Comments.Where(c => c.postId == postID);
+        //    }
+        //}
+
+        //public IEnumerable<Comment> getCommentsByPostID(int postID)
+        //{
+        //    {
+        //         return _context.Comments.Where(c => c.postId.Equals(postID));
+        //    }
+        //}
+
+        public Comment GetCommentById(int commentId)
+        {
+            return _context.Comments.FirstOrDefault(c => c.commentID == commentId);
+        }
+    }
+}
---
+++ CA3_TATJ_V2/Models/ICommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA3_TATJ_V2.Models
+{
+    public interface ICommentRepository
+    {
+        IEnumerable<Comment> allComments { get; }
+        Comment GetCommentById(int commentId);
+
+        //IEnumerable<Comment> getCommentsByPostID(int postID);
+    }
+}
